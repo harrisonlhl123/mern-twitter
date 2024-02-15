e10e149e88dee672ec11fbc460a724b2cbf675c0
Harrison Liang <liangharrison123@gmail.com>
2024-02-15 11:41:45 -0500

    like count showing up

--- frontend/src/components/Tweets/TweetBox.js
+++ frontend/src/components/Tweets/TweetBox.js
@@ -5,11 +5,44 @@ import { useDispatch, useSelector } from "react-redux";
 import { deleteTweet } from "../../store/tweets";
 import MakeComment from "../Comments/MakeComment";
 import AllComments from "../Comments/AllComments";
+import { getTweetLikes, deleteLike, createLike } from "../../store/like";
 
 function TweetBox ({ tweet: { _id, text, author }}) {
   const { username } = author;
   const dispatch = useDispatch();
   const currentUser = useSelector(state => state.session.user)
+
+  let likesForTweet = useSelector(getTweetLikes(_id))
+
+  let numOfLikes = likesForTweet.length ? (
+    <>
+        <i className="fa-solid fa-thumbs-up"></i> {likesForTweet.length}
+    </>
+  ) : null;
+
+  const userliked = likesForTweet.some((like) => like.user == currentUser?.id )
+  const likeByUser = likesForTweet.find((like) => like.user == currentUser?.id)
+
+  function handleUnlikeClick(e) {
+      e.preventDefault();
+
+      dispatch(deleteLike(likeByUser._id))
+  }
+
+  function handleLikeClick(e) {
+      e.preventDefault();
+
+      let like = {
+          user: currentUser._id,
+          likeable: _id,
+          likeableType: 'tweet'
+      }
+      dispatch(createLike(like))
+  }
+
+  const likeButtonStyle = {
+      color: userliked ? 'blue' : 'grey',
+  };
 
   const handleDelete = (e) => {
     e.preventDefault();
@@ -26,6 +59,11 @@ function TweetBox ({ tweet: { _id, text, author }}) {
           <button onClick={handleDelete}>Delete</button>
         </>
       )}
+      <>{numOfLikes}</>
+      <button onClick={userliked ? handleUnlikeClick : handleLikeClick}>
+        <i className="fa-solid fa-thumbs-up" style={likeButtonStyle}></i>
+        <p style={likeButtonStyle}>Like</p>
+      </button>
 
       <AllComments tweetId={_id} />
 
